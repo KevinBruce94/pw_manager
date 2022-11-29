0e07cd9f99b76cc57dbed81035fde3099ba572f5
Kevin Bruce <kevinvanleur@outlook.com>
2022-11-29 21:10:55 +0100

    Update PW_Manager_Full.py

--- PW_Manager_Full.py
+++ PW_Manager_Full.py
@@ -1,76 +1,110 @@
-import tkinter
 from tkinter import *
 from tkinter import messagebox
-import random
-import string
-import pandas
+import customtkinter
+from random import choice, randint, shuffle
+from PIL import ImageTk, Image
 import pyperclip
+import json
+
+LOGIN_FONT = "Roboto", 24, "bold"
+BOLD_FONT = "Roboto", 18, "bold"
+FONT = "Roboto", 16, "normal"
+PW_FONT = "Roboto", 12, "normal"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-def button_clicked():
-    length = 12
-    password_entry.delete(0, END)
-    characters = string.printable
-    generate_password = "".join(random.choice(characters) for num in range(length))
-    password_entry.insert(0, generate_password)
-    pyperclip.copy(generate_password)
+def generate_password():
+    p_entry.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    p_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    website = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
+    website = w_entry.get()
+    email = e_entry.get()
+    password = p_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please make sure you haven't leave any fields empty!")
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title="Password Manager", message=f"These are the details entered: \nEmail: {email}"
-                                                      f"\nPassword: {password} \n Is it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}" + "\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                #saving updated data
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            w_entry.delete(0, END)
+            e_entry.delete(0, END)
+            p_entry.delete(0, END)
 
-#Creating window
-window = Tk()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+# appearance
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+# create window + frame
+window = customtkinter.CTk()
 window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.geometry("600x400")
 
-#Logo
-canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
+frame = customtkinter.CTkFrame(master=window)
+frame.pack(padx=10, pady=10, fill="both", expand=True)
 
-#Labels
-website_label = Label(text="Website:", font=("Arial", 16, "bold"))
-website_label.grid(row=1, column=0)
+# # insert logo
+# logo = ImageTk.PhotoImage(Image.open("logo.png"))
+# label = customtkinter.CTkLabel(master=frame, image=logo)
+# label.grid(row=0, column=1, columnspan=2)
 
-email_label = Label(text="Email/Username:", font=("Arial", 16, "bold"))
-email_label.grid(row=2, column=0)
+# labels
+w_label = customtkinter.CTkLabel(master=frame, text="Website:", text_font=BOLD_FONT)
+w_label.grid(row=0, column=0, padx=10, pady=10)
+e_label = customtkinter.CTkLabel(master=frame, text="Email/Username:", text_font=BOLD_FONT)
+e_label.grid(row=1, column=0, padx=10, pady=10)
+p_label = customtkinter.CTkLabel(master=frame, text="Password:", text_font=BOLD_FONT)
+p_label.grid(row=2, column=0, padx=10, pady=10)
 
-password_label = Label(text="Password:", font=("Arial", 16, "bold"))
-password_label.grid(row=3, column=0)
+# Entries
+w_entry = customtkinter.CTkEntry(master=frame, text_font=FONT, width=350)
+w_entry.grid(row=0, column=1, columnspan=2, padx=30)
 
-#Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
-website_entry.focus()
+e_entry = customtkinter.CTkEntry(master=frame, text_font=FONT, width=350)
+e_entry.grid(row=1, column=1, columnspan=2, padx=30)
 
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "hans@outlook.com")
+p_entry = customtkinter.CTkEntry(master=frame, text_font=PW_FONT, text=generate_password)
+p_entry.grid(row=2, column=1, sticky=E, padx=20)
 
-password_entry = Entry(width=35)
-password_entry.grid(row=3, column=1, sticky="W")
+# button
+generate_pw = customtkinter.CTkButton(master=frame, text="Generate Password", text_font=FONT, command=generate_password)
+generate_pw.grid(row=2, column=2, sticky=W, padx=20)
 
-#Buttons
-generate_pw_button = Button(text="Generate Password", command=button_clicked)
-generate_pw_button.grid(row=3, column=1, sticky="E")
+add_button = customtkinter.CTkButton(master=frame, text="Add Password", text_font=FONT, width=150, command=save)
+add_button.grid(row=3, column=1, columnspan=2, pady=10)
 
-add_password_button = Button(text="Add", width=36, command=save)
-add_password_button.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
